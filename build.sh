126fef022933a549eecbd3db3913d077c43ffecf
userdocs <16525024+userdocs@users.noreply.github.com>
2023-02-19 20:18:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ git clone https://github.com/esnet/iperf.git "$HOME/iperf3_build"
 cd "$HOME/iperf3_build" || exit 1
 
 ./bootstrap.sh
-./configure --disable-shared --enable-static-bin --prefix="$HOME/iperf3"
+./configure --with-openssl=/usr --disable-shared --enable-static-bin --prefix="$HOME/iperf3"
 
 make
 
 [[ -d "$HOME/iperf3" ]] && rm -rf "$HOME/iperf3"
 make install
 
-cp -f "$HOME/system/bin/cygwin1.dll" "$HOME/iperf3/bin"
+[[ -d "$HOME/iperf3/bin" ]] && cp -f "$HOME/system/bin/cygwin1.dll" "$HOME/iperf3/bin"
