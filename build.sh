3d47b87ef5f2c1f3f81746501519025090dd29c3
userdocs <16525024+userdocs@users.noreply.github.com>
2023-02-19 01:35:04 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+HOME="${1}"
+
 pwd
 
-git clone https://github.com/esnet/iperf.git "iperf3_build"
-cd "iperf3_build" || exit 1
+git clone https://github.com/esnet/iperf.git "$HOME/iperf3_build"
+cd "$HOME/iperf3_build" || exit 1
 
 ./bootstrap.sh
-./configure --disable-shared --enable-static-bin --prefix="/iperf3"
+./configure --disable-shared --enable-static-bin --prefix="$HOME/iperf3"
 
 make
 make install
