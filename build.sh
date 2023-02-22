b2afd0c7e2f796c684657c435ef32779273c78d3
userdocs <16525024+userdocs@users.noreply.github.com>
2023-02-22 02:20:42 +0000

    test

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-with_openssl="${1:-no}"
-cygwin_path="${2}"
 HOME="$(pwd)"
+with_openssl="${1:-no}"
+cygwin_path="${2:-${HOME}}"
 
 printf '\n%b\n' " \e[93m\U25cf\e[0m With openssl = ${with_openssl}"
 printf '\n%b\n' " \e[93m\U25cf\e[0m Build path = ${HOME}"
