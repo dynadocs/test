3e675ff5705b017ad5f91cceb36db63f9de5b222
userdocs <16525024+userdocs@users.noreply.github.com>
2023-02-22 01:15:13 +0000

    test

--- build.sh
+++ build.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
-# The matrix will pass this as yes or no to the build script - defaults to no but the build will fail if you have libssl-devel installed.
 with_openssl="${1:-no}"
-printf '\n%b\n' " \e[93m\U25cf\e[0m Building with openssl = ${with_openssl}"
 HOME="$(pwd)"
+
+if [[ "${with_openssl}" == 'yes' ]]; then
+	printf '\n%b\n' " \e[93m\U25cf\e[0m Downloading zlib"
+	curl -sL https://github.com/madler/zlib/releases/download/v1.2.13/zlib-1.2.13.tar.gz >zlib.tar.gz
+	printf '\n%b\n' " \e[93m\U25cf\e[0m Extracting zlib"
+	tar xf zlib.tar.gz
+	cd "${HOME}/zlib-1.2.13" || exit 1
+	printf '\n%b\n' " \e[93m\U25cf\e[0m Configuring zlib"
+	./configure --prefix="${HOME}/cygwin" --static
+	printf '\n%b\n' " \e[93m\U25cf\e[0m Building with zlib"
+	make -j"$(nproc)"
+	make install
+	printf '\n%b\n' " \e[93m\U25cf\e[0m Building with openssl"
+	printf '\n%b\n\n' " \e[94m\U25cf\e[0m Downloading openssl"
+	curl -L "https://github.com/userdocs/qbt-workflow-files/releases/latest/download/openssl.tar.xz" >openssl.tar.xz
+	printf '\n%b\n\n' " \e[94m\U25cf\e[0m Extracting openssl"
+	tar xf openssl.tar.xz
+	cd $"{HOME}/openssl" || exit 1
+	printf '\n%b\n\n' " \e[94m\U25cf\e[0m Building openssl"
+	./config --prefix="${HOME}/cygwin" threads no-shared no-dso no-comp
+	make -j"$(nproc)"
+	make install_sw
+fi
 
 printf '\n%b\n\n' " \e[94m\U25cf\e[0m Cloning iperf3 git repo"
 
@@ -12,22 +33,14 @@ git clone "https://github.com/esnet/iperf.git" "$HOME/iperf3_build"
 cd "$HOME/iperf3_build" || exit 1
 
 printf '\n%b\n' " \e[92m\U25cf\e[0m Setting iperf3 version to file iperf3_version"
-sed -rn 's|(.*)\[(.*)],\[https://github.com/esnet/iperf],(.*)|\2|p' configure.ac > "$HOME/iperf3_version"
+sed -rn 's|(.*)\[(.*)],\[https://github.com/esnet/iperf],(.*)|\2|p' configure.ac >"$HOME/iperf3_version"
 
 printf '\n%b\n\n' " \e[94m\U25cf\e[0m Bootstrapping iperf3"
 
 ./bootstrap.sh
 
 printf '\n%b\n\n' " \e[94m\U25cf\e[0m Configuring iperf3"
-
-if [[ "${with_openssl}" = 'yes' ]]; then
-	# fix openssl linking - We want this sed replace using a literal match
-	# shellcheck disable=SC2016
-	sed -ri 's|OPENSSL_LIBS="-lssl -lcrypto"|OPENSSL_LIBS="${OPENSSL_LIBS}"|g' "$HOME/iperf3_build/configure"
-	./configure --with-openssl=/usr OPENSSL_LIBS="-l:libssl.dll.a -l:libcrypto.dll.a" --disable-shared --enable-static-bin --prefix="$HOME/iperf3"
-else
-	./configure --disable-shared --enable-static-bin --prefix="$HOME/iperf3"
-fi
+./configure --disable-shared --enable-static --enable-static-bin --prefix="$HOME/iperf3"
 
 printf '\n%b\n\n' " \e[94m\U25cf\e[0m make"
 make -j"$(nproc)"
@@ -45,14 +58,5 @@ if [[ -d "$HOME/iperf3/bin" ]]; then
 	# action
 	[[ -f "/cygdrive/c/cygwin/bin/cygwin1.dll" ]] && cp -f "/cygdrive/c/cygwin/bin/cygwin1.dll" "$HOME/iperf3/bin"
 
-	if [[ "${with_openssl}" == 'yes' ]]; then
-		# openssl requirements
-		# cmd
-		[[ -f "$HOME/cygwin/bin/cygcrypto-1.1.dll" ]] && cp -f "$HOME/cygwin/bin/cygcrypto-1.1.dll" "$HOME/iperf3/bin"
-		[[ -f "$HOME/cygwin/bin/cygz.dll" ]] && cp -f "$HOME/cygwin/bin/cygz.dll" "$HOME/iperf3/bin"
-		# action
-		[[ -f "/cygdrive/c/cygwin/bin/cygcrypto-1.1.dll" ]] && cp -f "/cygdrive/c/cygwin/bin/cygcrypto-1.1.dll" "$HOME/iperf3/bin"
-		[[ -f "/cygdrive/c/cygwin/bin/cygz.dll" ]] && cp -f "/cygdrive/c/cygwin/bin/cygz.dll" "$HOME/iperf3/bin"
-	fi
 	printf '\n%b\n\n' " \e[92m\U25cf\e[0m Copied the dll dependencies"
 fi
